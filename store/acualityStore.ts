22199b83489d89f0b0f2b5ed790cee5ece69f2dd
Petr Hanzl <petrhanzl33@gmail.com>
2023-05-30 07:24:51 +0200

    Add actualityStore

---
+++ store/acualityStore.ts
@@ -0,0 +1,72 @@
+import {defineStore} from "pinia";
+import supabase from "~/supabase/client";
+import {Actuality, ActualityType} from "~/types/collection";
+
+
+export const useAuthStore = defineStore("actualityStore", {
+    state: () => ({
+        actualities: [] as Actuality[],
+        actualityTypes: [] as ActualityType[]
+
+    }),
+    actions: {
+        async fetchAllActualityTypes(force: boolean) {
+
+            // do we need to fetch?
+            if (!force && this.actualityTypes && this.actualityTypes.length > 0) {
+                return
+            }
+
+            const { data, error } = await supabase
+                .from("actuality_types")
+                .select()
+
+            if (error) {
+                console.log("error")
+                console.log(error)
+            }
+
+            if (data) {
+                console.log("data")
+                console.log(data)
+                this.actualityTypes = data
+            }
+
+
+        },
+
+        async fetchAllActualities(force: boolean) {
+
+            // prefetch types
+            await this.fetchAllActualityTypes(force)
+
+            // do we need to fetch?
+            if (!force && this.actualities && this.actualities.length > 0) {
+                return
+            }
+
+            const { data, error } = await supabase
+                .from("actualities")
+                .select()
+            if (error) {
+                console.log("error")
+                console.log(error)
+            }
+
+            if (data) {
+                console.log("data")
+                console.log(data)
+                this.actualities = data
+            }
+        }
+    },
+    getters: {
+        getActualities(): Actuality[] {
+            return this.actualities
+        },
+        getActualityTypes(): ActualityType[] {
+            return this.actualityTypes
+        }
+
+    }
+})
