79c3861e42ff216ba221db103bd28e7e20e80894
Petr Hanzl <petrhanzl33@gmail.com>
2023-06-04 16:50:42 +0200

    Add index file

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+      <LoginForm/>
+  </v-app>
+
+</template>
+
+<script setup lang="ts">
+
+    definePageMeta({
+        middleware: ["auth"]
+    })
+
+</script>
+
+<style scoped>
+
+</style>
