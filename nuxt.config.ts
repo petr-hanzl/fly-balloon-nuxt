83ba2b4ff1a1e6c054275c3b1f608f2b0471fb89
Petr Hanzl <petrhanzl33@gmail.com>
2023-06-06 16:03:48 +0200

    Use official vuetify distro for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,30 @@
 
 
+
 export default defineNuxtConfig({
     modules: [
-        '@invictus.codes/nuxt-vuetify',
         '@pinia/nuxt',
         '@nuxtjs/supabase',
         // '@nuxt/image',
         'nuxt-icon'
     ],
+    devServerHandlers: [],
     supabase: {
 
     },
+    css: [
+        'vuetify/lib/styles/main.sass',
+        // '@mdi/font/css/materialdesignicons.min.css', todo this might be needed for material icons
+    ],
+    build: {
+        transpile: ['vuetify'],
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
+    },
+    // @ts-ignore
     vuetify: {
         /* vuetify options */
         vuetifyOptions: {
